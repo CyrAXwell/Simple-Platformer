872822645cbc2c463f6ccc9a91a695776b27fc75
CyrAXwell <pogrebnoy_y@mail.ru>
2024-03-12 08:53:10 +0300

    Add chek groun for jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,15 +7,15 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed = 10f;
     [SerializeField] private float gravityScale = 10f;
     [SerializeField] private float jumpCooldown = 0.2f;
+    [SerializeField] private LayerMask whatIsGround;
 
     private Rigidbody2D _rb;
     private float _jumpCooldownCounter;
-    private bool grounded;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
-
+        _rb.gravityScale = gravityScale;
     }
 
     private void Update()
@@ -24,16 +24,20 @@ public class PlayerMovement : MonoBehaviour
         _jumpCooldownCounter -= Time.deltaTime;
     }
 
+    private void Move() =>
+        _rb.velocity = new Vector2( speed, _rb.velocity.y);
+
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.performed && grounded)
-        {
+        if (context.performed && IsGrounded() && _jumpCooldownCounter <= 0f)
             _rb.gravityScale *= -1f;
-        } 
     }
 
-    private void Move()
+    private bool IsGrounded()
     {
-        _rb.velocity = new Vector2( speed, _rb.velocity.y);
+        RaycastHit2D rayCastHitUpward = Physics2D.Raycast(transform.position, Vector2.up, transform.localScale.y / 2f + 0.2f, whatIsGround);
+        RaycastHit2D rayCastHitDownward = Physics2D.Raycast(transform.position, Vector2.down, transform.localScale.y / 2f + 0.2f, whatIsGround);
+
+        return rayCastHitUpward.collider != null || rayCastHitDownward.collider != null;
     }
 }
